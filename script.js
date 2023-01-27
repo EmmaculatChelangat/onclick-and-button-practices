a36bacdc8ce4f4b07fc60cd215663e801549c271
Chelangat Emmaculat <chelangatemmaculat2744@gmail.com>
2023-01-27 13:55:08 +0300

    button and onclick practices

---
+++ script.js
@@ -0,0 +1,6 @@
+function freeCourse(){
+    var fCourse = document.querySelector('.btn1').innerHTML;
+    var pCourse = document.querySelector('.btn2').value;
+    document.write(fCourse);
+    // document.write('button name' + pCourse);
+}
